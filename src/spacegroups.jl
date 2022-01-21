9793fb9c792ffcde3d73c55768223a9085e4754c
Brandon Flores <bsflores@wisc.edu>
2022-01-21 13:32:26 -0600

    Removed irrelevant code - will rethink this later?

--- src/spacegroups.jl
+++ src/spacegroups.jl
@@ -1,14 +1,2 @@
-"""
-    SetSpaceGroup{N}
-
-Space group with information about the origin and orientation of the main rotational axis.
-"""
-struct SetSpaceGroup{N}
-    # The actual space group
-    spgrp::Crystalline.SpaceGroup{N}
-    # Normally this is the point of inversion
-    # But this varies for non-centrosymmetric space groups
-    origin::SVector{N,Float64}
-    # Number of the axis
-    index::SVector{N,Int}
-end
+# TODO: Implement methods for processing space groups by notation
+# This includes automatic detection of origin, unique axis, and other data
